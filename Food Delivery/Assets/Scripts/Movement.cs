427baf52d7685d5a72a139f94bce78acce600086
Brantor <erik@moree.se>
2023-01-15 18:07:32 +0100

    Revert "Revert "as<d""
    This reverts commit f5062e462b5f92ba086b31a97d75a53e4f010d95.

--- Food Delivery/Assets/Scripts/Movement.cs
+++ Food Delivery/Assets/Scripts/Movement.cs
@@ -13,5 +13,11 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         _rb.velocity = new Vector3(_js.Horizontal * _ms, _rb.velocity.y, _js.Vertical * _ms);
+
+        if (Math.Abs(_js.Horizontal) > 0.001f || Math.Abs(_js.Vertical) > 0.001f)
+        {
+            float heading = Mathf.Atan2(-_js.Horizontal, -_js.Vertical);
+            transform.rotation = Quaternion.Euler(0f, heading * Mathf.Rad2Deg + 180, 0f);
+        }
     }
 }
